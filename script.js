144b2d438f8a42e18467a165b4aca2f398e6f383
kaiqueftz <kaique.anjos@sempreceub.com>
2024-04-03 10:48:13 -0300

    Atualização 03/04/24

--- script.js
+++ script.js
@@ -30,8 +30,6 @@ function limpar() {
     paragrafoResultado.textContent = "";
 
     let valor = document.getElementById("valorEntrada");
-    valorEntrada.value = "";
-
 }
 
 function inverter() {
@@ -67,14 +65,23 @@ function converter() {
     let paragrafoResultado = document.getElementById("resultado");
     paragrafoResultado.textContent = simbolo + " " + resultado.toFixed(2);
 
-    // Adicionando moedas ao container
+    // Remover moedas existentes antes de adicionar novas
     const moedasContainer = document.getElementById("moedas");
-    const numeroMoedas = Math.floor(Math.random() * 5) + 1; // Gerando um número aleatório de moedas
+    moedasContainer.innerHTML = "";
+
+    // Adicionando moedas ao container de acordo com o valor digitado pelo usuário
+    const numeroMoedas = Math.min(valorUsuario, 200); // Limitando a um máximo de 1000 moedas
     for (let i = 0; i < numeroMoedas; i++) {
         const moeda = document.createElement("div");
         moeda.classList.add("moeda");
         moeda.style.left = `${Math.random() * 90}%`; // Posicionando aleatoriamente na largura do container
         moeda.textContent = "$"; // Adicionando o cifrão como texto dentro da div
+        moeda.style.animationDuration = `${Math.random() * 5 + 3}s`; // Definindo tempos de queda aleatórios
         moedasContainer.appendChild(moeda);
+
+        // Definindo tempo para a animação de retorno
+        setTimeout(() => {
+            moeda.classList.add("volta");
+        }, (Math.random() * 100000) + 10000); // Tempo aleatório entre 5 e 10 segundos
     }
 }
